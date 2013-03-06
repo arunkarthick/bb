9b76d6c3122cf2f08e87d871e3f9f81aad3e0e09
arunkarthick <arunkarthick.it@gmail.com>
2013-03-05 23:13:09 -0500

    Adding monkey patch to support nulls last in postgres

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,7 +1,7 @@
 class PlayersController < ApplicationController
   active_scaffold :player do |conf|
     conf.columns = [:league, :division, :given_name, :surname, :team, :average, :hr, :rbi, :runs, :sb, :ops]
-    list.sorting = {:average => 'DESC'}
+    list.sorting = {:average => 'DESC NULLS LAST'}
     conf.actions = [:list]
     list.per_page = DEFAULT_NUMBER_OF_LIST_ITEMS
     conf.columns[:hr].label = "HR"
--- config/initializers/active_scaffold.rb
+++ config/initializers/active_scaffold.rb
@@ -1,1 +1,27 @@
 DEFAULT_NUMBER_OF_LIST_ITEMS = 25
+
+module ActiveScaffold::DataStructures
+  class Sorting
+
+  	def add(column_name, direction = nil)
+      direction ||= 'ASC'
+      direction = direction.to_s.upcase
+      column = get_column(column_name)
+      raise ArgumentError, "Sorting direction unknown" unless [:ASC, :DESC, :"DESC NULLS LAST", :"ASC NULLS LAST"].include? direction.to_sym
+      @clauses << [column, direction.untaint] if column and column.sortable?
+      raise ArgumentError, "Can't mix :method- and :sql-based sorting" if mixed_sorting?
+    end
+
+    private
+    def extract_direction(direction_part)
+      val = direction_part.to_s.upcase
+      if val == 'DESC'
+        'DESC'
+      elsif val == 'ASC'
+        'ASC'
+      else
+      	val
+      end
+    end
+  end
+end
